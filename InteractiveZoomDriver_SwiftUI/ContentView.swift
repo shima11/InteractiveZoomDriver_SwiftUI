3fc21d20c3aba4c1afda6179a08cdee5c9664df8
shima11 <shima.jin@icloud.com>
2022-12-07 17:27:28 +0900

    cleanup

--- InteractiveZoomDriver_SwiftUI/ContentView.swift
+++ InteractiveZoomDriver_SwiftUI/ContentView.swift
@@ -19,10 +19,8 @@ struct ContentView: View {
 
             VStack(spacing: 40) {
 
-              // TODO: 今のところ複数同時に存在しているとうまく動かない（PreferenceKeyがうまく対応できていない）
               Color.blue
                 .frame(width: 200, height: 100)
-                .addPinchZoom()
 
               Color.orange
                 .frame(width: 200, height: 200)
@@ -55,76 +53,5 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
     ContentView()
-  }
-}
-
-extension View {
-  func addPinchZoom() -> some View {
-    PinchZoomContext {
-      self
-    }
-  }
-}
-
-struct PinchZoomContext<Content: View>: View {
-
-  private let content: Content
-
-  @State private var localOriginalFrame: CGRect = .zero
-  @State private var originalFrame: CGRect = .zero
-
-  @State private var localOffset: CGPoint = .zero
-  @State private var offset: CGSize = .zero
-
-  @State private var scale: CGFloat = 1
-
-  @State private var scalePosition: CGPoint = .zero
-  @State private var isPinching: Bool = false
-
-  @State private var show: Bool = false
-
-  @State var overlayView: EquatableViewContainer? = nil
-
-  @State var opacity: CGFloat = 1
-
-  init(@ViewBuilder content: @escaping () -> Content) {
-    self.content = content()
-  }
-
-  var body: some View {
-    content
-      .overlay(
-        ZoomGestureView(scale: $scale, offset: $localOffset, scalePosition: $scalePosition, isPinching: $isPinching)
-      )
-    // overlayとbackgroundの組み合わせでgeometryreaderが動かないケースがあるみたい
-      .overlay(GeometryReader { proxy in
-        Color.clear
-          .preference(key: FramePreferenceKey.self, value: proxy.frame(in: .global))
-      })
-      .opacity(opacity)
-    // ちらつきを防止するためにdelayを入れている
-      .animation(opacity > 0 ? nil : .default.delay(0.1), value: opacity)
-      .animation(isPinching ? nil : .spring(), value: scale)
-      .animation(isPinching ? nil : .spring(), value: offset)
-    // scaleのアニメーションの終了を取得して、全画面の表示の終了を判断
-      .modifier(AnimatableModifierDouble(bindedValue: scale, completion: {
-        show = (scale > 1) || isPinching
-      }))
-    // Pinchが開始したら全画面表示を開始する
-      .onChange(of: scale, perform: { newValue in
-        guard show == false else { return }
-        show = (newValue > 1) || isPinching
-      })
-      .onChange(of: show, perform: { newValue in
-        overlayView = newValue ? .init(view: AnyView(content)) : nil
-        opacity = newValue ? 0 : 1
-        offset = .init(width: localOffset.x, height: localOffset.y)
-        originalFrame = localOriginalFrame
-      })
-      .preference(key: OffsetPreferenceKey.self, value: offset)
-      .preference(key: ScalePreferenceKey.self, value: scale)
-      .preference(key: AnyViewPreferenceKey.self, value: overlayView)
-      .preference(key: ZoomingPreferenceKey.self, value: show)
-      .preference(key: PinchingPreferenceKey.self, value: isPinching)
   }
 }
---
+++ InteractiveZoomDriver_SwiftUI/InteractiveZoomDriver.swift
@@ -0,0 +1,315 @@
+//
+//  InteractiveZoomDriver.swift
+//  InteractiveZoomDriver_SwiftUI
+//
+//  Created by Jinsei Shima on 2022/12/07.
+//
+
+import SwiftUI
+
+struct InteractiveZoomContainer<Content: View>: View {
+
+  @State var zoomScale: CGFloat = 1
+  @State var originalFrame: CGRect = .zero
+  @State var offset: CGSize = .zero
+  @State var overlayView: AnyView? = nil
+  @State var zooming: Bool = false
+  @State var pinching: Bool = false
+
+  let content: Content
+
+  init(@ViewBuilder content: () -> Content) {
+    self.content = content()
+  }
+
+  var body: some View {
+    ZStack {
+
+      content
+
+      Color.black.opacity(min((zoomScale - 1) * 0.5, 0.5))
+        .ignoresSafeArea()
+        .allowsHitTesting(false)
+
+      overlayView?
+        .opacity(zooming ? 1 : 0)
+      // TODO: 微妙に元のViewと位置がずれてしまう(12ずれてるっぽい)
+        .offset(
+          x: offset.width/2 + originalFrame.midX - UIScreen.main.bounds.width/2,
+          y: offset.height/2 + originalFrame.midY - UIScreen.main.bounds.height/2 - 12
+        )
+        .scaleEffect(zoomScale)
+        .animation(pinching ? nil : .spring(), value: zoomScale)
+        .animation(pinching ? nil : .spring(), value: offset)
+        .ignoresSafeArea()
+        .allowsHitTesting(false)
+    }
+    .onPreferenceChange(OffsetPreferenceKey.self) { value in
+      print("ofset:", value ?? .zero)
+      offset = value ?? .zero
+    }
+    .onPreferenceChange(ScalePreferenceKey.self) { value in
+      print("scale:", value ?? .zero)
+      zoomScale = value ?? 1
+    }
+    .onPreferenceChange(AnyViewPreferenceKey.self) { value in
+      print("view:", value?.view ?? "")
+      overlayView = value?.view
+    }
+    .onPreferenceChange(FramePreferenceKey.self) { value in
+      print("frame:", value ?? "")
+      originalFrame = value ?? .zero
+    }
+    .onPreferenceChange(ZoomingPreferenceKey.self) { value in
+      print("zooming:", value ?? "")
+      zooming = value ?? false
+    }
+    .onPreferenceChange(PinchingPreferenceKey.self) { value in
+      print("pinching:", value ?? "")
+      pinching = value ?? false
+    }
+  }
+}
+
+extension View {
+  func addPinchZoom() -> some View {
+    PinchZoomContext {
+      self
+    }
+  }
+}
+struct PinchZoomContext<Content: View>: View {
+
+  private let content: Content
+
+  @State private var originalFrame: CGRect = .zero
+
+  @State private var localOffset: CGPoint = .zero
+
+  @State private var scale: CGFloat = 1
+
+  @State private var scalePosition: CGPoint = .zero
+  @State private var isPinching: Bool = false
+
+  @State private var show: Bool = false
+
+  @State var overlayView: EquatableViewContainer? = nil
+
+  @State var opacity: CGFloat = 1
+
+  init(@ViewBuilder content: () -> Content) {
+    self.content = content()
+  }
+
+  var body: some View {
+    content
+      .overlay(
+        ZoomGestureView(scale: $scale, offset: $localOffset, scalePosition: $scalePosition, isPinching: $isPinching)
+      )
+    // overlayとbackgroundの組み合わせでgeometryreaderが動かないケースがあるみたい
+      .overlay(GeometryReader { proxy in
+        Color.clear
+          .preference(key: FramePreferenceKey.self, value: proxy.frame(in: .global))
+      })
+      .opacity(opacity)
+    // ちらつきを防止するためにdelayを入れている
+      .animation(opacity > 0 ? nil : .default.delay(0.1), value: opacity)
+      .animation(isPinching ? nil : .spring(), value: scale)
+      .animation(isPinching ? nil : .spring(), value: localOffset)
+    // scaleのアニメーションの終了を取得して、全画面の表示の終了を判断
+      .modifier(AnimatableModifierDouble(bindedValue: scale, completion: {
+        show = (scale > 1) || isPinching
+      }))
+    // Pinchが開始したら全画面表示を開始する
+      .onChange(of: scale, perform: { newValue in
+        guard show == false else { return }
+        show = (newValue > 1) || isPinching
+      })
+      .onChange(of: show, perform: { newValue in
+        overlayView = newValue ? .init(view: AnyView(content)) : nil
+        opacity = newValue ? 0 : 1
+      })
+      .preference(key: OffsetPreferenceKey.self, value: .init(width: localOffset.x, height: localOffset.y))
+      .preference(key: ScalePreferenceKey.self, value: scale)
+      .preference(key: AnyViewPreferenceKey.self, value: overlayView)
+      .preference(key: ZoomingPreferenceKey.self, value: show)
+      .preference(key: PinchingPreferenceKey.self, value: isPinching)
+  }
+}
+
+struct ZoomGestureView: UIViewRepresentable {
+
+  @Binding var scale: CGFloat
+  @Binding var offset: CGPoint
+  @Binding var scalePosition: CGPoint
+  @Binding var isPinching: Bool
+
+  func makeUIView(context: Context) -> UIView {
+    let view = UIView()
+    view.backgroundColor = .clear
+    let pinchGesture = UIPinchGestureRecognizer(
+      target: context.coordinator,
+      action: #selector(context.coordinator.hundlePinch(sender:))
+    )
+    view.addGestureRecognizer(pinchGesture)
+    let panGesture = UIPanGestureRecognizer(
+      target: context.coordinator,
+      action: #selector(context.coordinator.hundlePan(sender:))
+    )
+    panGesture.maximumNumberOfTouches = 2
+    panGesture.delegate = context.coordinator
+    view.addGestureRecognizer(panGesture)
+    return view
+  }
+
+  func updateUIView(_ uiView: UIViewType, context: Context) {
+  }
+
+  func makeCoordinator() -> Coordinator {
+    Coordinator(parent: self)
+  }
+
+  final class Coordinator: NSObject, UIGestureRecognizerDelegate {
+
+    private var parent: ZoomGestureView
+
+    init(parent: ZoomGestureView) {
+      self.parent = parent
+    }
+
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+      return true
+    }
+
+    @objc func hundlePinch(sender: UIPinchGestureRecognizer) {
+
+      if sender.state == .began || sender.state == .changed, sender.scale > 1 {
+
+        parent.isPinching = true
+
+        withAnimation(.spring()) {
+          parent.scale = sender.scale
+        }
+
+        if parent.scalePosition == .zero {
+          let scalePoint: CGPoint =  .init(
+            x: sender.location(in: sender.view).x / sender.view!.bounds.width,
+            y: sender.location(in: sender.view).y / sender.view!.bounds.height
+          )
+          parent.scalePosition = scalePoint
+        }
+
+      } else {
+
+        parent.isPinching = false
+
+        withAnimation(.spring()) {
+          parent.scale = 1
+          parent.scalePosition = .zero
+        }
+      }
+    }
+
+    @objc func hundlePan(sender: UIPanGestureRecognizer) {
+      if sender.state == .began || sender.state == .changed && parent.scale > 1 {
+        withAnimation(.spring()) {
+          parent.offset = sender.translation(in: sender.view)
+        }
+      } else {
+        withAnimation(.spring()) {
+          parent.offset = .zero
+          parent.scalePosition = .zero
+        }
+      }
+    }
+  }
+}
+
+// withAnimationのcompletionを取得するため
+struct AnimatableModifierDouble: AnimatableModifier {
+
+  private var targetValue: Double
+
+  var animatableData: Double {
+    didSet {
+      checkIfFinished()
+    }
+  }
+
+  var completion: () -> ()
+
+  init(bindedValue: Double, completion: @escaping () -> ()) {
+    self.completion = completion
+    self.animatableData = bindedValue
+    targetValue = bindedValue
+  }
+
+  func checkIfFinished() -> () {
+    if (animatableData == targetValue) {
+      DispatchQueue.main.async {
+        self.completion()
+      }
+    }
+  }
+
+  func body(content: Content) -> some View {
+    content
+      .animation(nil)
+  }
+}
+
+struct EquatableViewContainer: Equatable {
+  let id = UUID().uuidString
+  let view: AnyView?
+  static func == (lhs: EquatableViewContainer, rhs: EquatableViewContainer) -> Bool {
+    return lhs.id == rhs.id
+  }
+}
+
+struct AnyViewPreferenceKey: PreferenceKey {
+  typealias Value = EquatableViewContainer?
+  static var defaultValue: Value = nil
+  static func reduce(value: inout Value, nextValue: () -> Value) {
+    value = nextValue()
+  }
+}
+
+struct FramePreferenceKey: PreferenceKey {
+  typealias Value = CGRect?
+  static var defaultValue: Value = nil
+  static func reduce(value: inout Value, nextValue: () -> Value) {
+    value = nextValue()
+  }
+}
+
+struct OffsetPreferenceKey: PreferenceKey {
+  typealias Value = CGSize?
+  static var defaultValue: Value = nil
+  static func reduce(value: inout Value, nextValue: () -> Value) {
+    value = nextValue()
+  }
+}
+struct ScalePreferenceKey: PreferenceKey {
+  typealias Value = CGFloat?
+  static var defaultValue: Value = nil
+  static func reduce(value: inout Value, nextValue: () -> Value) {
+    value = nextValue()
+  }
+}
+
+struct ZoomingPreferenceKey: PreferenceKey {
+  typealias Value = Bool?
+  static var defaultValue: Value = nil
+  static func reduce(value: inout Value, nextValue: () -> Value) {
+    value = nextValue()
+  }
+}
+
+struct PinchingPreferenceKey: PreferenceKey {
+  typealias Value = Bool?
+  static var defaultValue: Value = nil
+  static func reduce(value: inout Value, nextValue: () -> Value) {
+    value = nextValue()
+  }
+}
+
