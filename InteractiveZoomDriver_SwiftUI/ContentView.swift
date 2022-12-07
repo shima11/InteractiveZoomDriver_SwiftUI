761804d357115ffba48d43c676eea36d5b044066
shima11 <shima.jin@icloud.com>
2022-12-07 16:14:15 +0900

    Initial Commit

---
+++ InteractiveZoomDriver_SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  InteractiveZoomDriver_SwiftUI
+//
+//  Created by Jinsei Shima on 2022/12/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InteractiveZoomDriver_SwiftUI/InteractiveZoomDriver_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveZoomDriver_SwiftUIApp.swift
+//  InteractiveZoomDriver_SwiftUI
+//
+//  Created by Jinsei Shima on 2022/12/07.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveZoomDriver_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
